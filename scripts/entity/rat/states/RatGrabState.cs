d2256b6e9589af255d623dd630e36feaee46b80c
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-07-22 23:30:43 +0200

    chore: implemented RatGrabState

--- scripts/entity/rat/states/RatGrabState.cs
+++ scripts/entity/rat/states/RatGrabState.cs
@@ -1,8 +1,20 @@
+using Godot;
+
 public class RatGrabState : RatState
 {
-    public RatGrabState(Rat owner) : base(owner) { }
+    private Node _prevParent;
+    private Player _player;
+    public RatGrabState(Rat owner, Player player) : base(owner) { _player = player; }
     public override void PhysicsProcess(float delta) { }
     public override void Process(float delta) { }
-    public override void Enter(State previous = null) { }
-    public override void Exit() { }
+    public override void Enter(State previous = null)
+    {
+        _prevParent = _rat.GetParent();
+        _rat.GlobalPosition = _player.ThrowAnchor.GlobalPosition;
+        _rat.Reparent(_player.ThrowAnchor);
+    }
+    public override void Exit()
+    {
+        _rat.Reparent(_prevParent);
+    }
 }
