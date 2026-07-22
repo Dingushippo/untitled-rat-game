0724634e7fb5d7b4c1579c97940249c365b6a773
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-07-22 23:31:22 +0200

    chore: added GrabComponent baseline

---
+++ scripts/player/GrabComponent.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+public class GrabComponent
+{
+    const float GRAB_DISTANCE = 1f;
+    private Player _player;
+
+    public GrabComponent(Player player)
+    {
+        _player = player;
+    }
+
+    public void Update()
+    {
+
+    }
+}
