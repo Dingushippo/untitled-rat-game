132c4e93d8271fa08a1b30d9eea4839043a39ee8
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-07-27 20:20:29 +0200

    feat: add rat throw tuning scene

---
+++ scripts/singletons/Game/RatThrowTuning.cs
@@ -0,0 +1,50 @@
+using Godot;
+
+public partial class RatThrowTuning : Node3D
+{
+    [Export] public Player Player;
+    [Export] public Rat rat;
+
+    private float _ratFlyCounter = 0;
+    private Vector3 _startingPosition;
+    private bool _counterEnabled = false;
+
+
+    public override void _Ready()
+    {
+        EventBus.Subscribe(Event.RatThrown, OnRatThrown);
+        EventBus.Subscribe(Event.RatLanded, OnRatLanded);
+        GD.Print("Ready");
+    }
+
+    public override void _Process(double delta)
+    {
+        if (!_counterEnabled) return;
+
+        _ratFlyCounter += (float)delta;
+    }
+
+    private void OnRatThrown(params object[] args)
+    {
+        GD.Print("Throw");
+        _startingPosition = Player.GlobalPosition;
+        _counterEnabled = true;
+    }
+
+    private void OnRatLanded(params object[] args)
+    {
+        _counterEnabled = false;
+        float distance = _startingPosition.DistanceTo(rat.GlobalPosition);
+        GD.Print($"Traveled {distance}m in {_ratFlyCounter} seconds");
+
+        _ratFlyCounter = 0;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventKey key && key.Pressed && key.Keycode == Key.Key1)
+        {
+            Player.GrabComponent.InjectGrabState(rat);
+        }
+    }
+}
