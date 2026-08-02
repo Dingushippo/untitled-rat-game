e8641147485bc0917e31139b532070e78367cbaa
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-02 18:33:04 +0200

    chore: add physics layers static class

---
+++ scripts/core/PhysicsLayers.cs
@@ -0,0 +1,23 @@
+
+
+using Godot;
+
+public static class PhysicsLayers
+{
+    public static uint WORLD = 1;
+    public static uint PLAYER = 2;
+    public static uint INTERACT = 4;
+    public static uint ENTITY = 8;
+    public static uint FACILITY = 16;
+
+    public static uint GetOrMask(params int[] layers)
+    {
+        if (layers.IsEmpty()) return 0;
+        uint mask = 0;
+        foreach (int layer in layers)
+        {
+            mask |= (uint)layer;
+        }
+        return mask;
+    }
+}
