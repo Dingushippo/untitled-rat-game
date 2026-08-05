6ccf22bab4d8f58879bc220e4f66aca380d406a6
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-05 10:27:47 +0200

    chore: add static qte action class

--- scripts/qte/QteBase.cs
+++ scripts/qte/QteBase.cs
@@ -7,14 +7,26 @@ public partial class QteBase : Control
 
     public override void _Ready()
     {
-        EventBus.Publish(Event.QTEStarted, this);
+        EventBus.Publish(Event.QteStarted, this);
         GD.Print("Started QTE");
     }
 
     protected void OnCompleted(bool success)
     {
         Completed?.Invoke(success);
-        EventBus.Publish(Event.QTECompleted, this);
+        EventBus.Publish(Event.QteCompleted, this);
         QueueFree();
+    }
+}
+
+public static class QteActions
+{
+    public static string[] ValidActions = { "interact", "move_forward", "move_left", "move_right", "move_back", "jump" };
+
+    public static string GetRandomAction()
+    {
+        Random rand = new();
+        int index = rand.Next(ValidActions.Length);
+        return ValidActions[index];
     }
 }
