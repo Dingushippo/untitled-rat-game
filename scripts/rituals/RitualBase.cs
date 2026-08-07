122a8b2342dad094617ff5d4461eec48e8d9987c
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-08 00:29:24 +0200

    chore: fix redraw issues on ritual base

--- scripts/rituals/RitualBase.cs
+++ scripts/rituals/RitualBase.cs
@@ -5,19 +5,28 @@ using System.ComponentModel;
 [GlobalClass, Tool]
 public partial class RitualBase : Node3D
 {
+    private const float DRAW_FREQ = 60f;
     // Called when the node enters the scene tree for the first time.
     [Export] public SubViewport Viewport;
     [Export] public RitualRenderer Renderer;
     [Export] public RitualResource RitualResource;
     [Export] public MeshInstance3D PlaneMesh;
+
+
+    private float timer = 0;
     public override void _Ready()
     {
         Renderer.Position = Viewport.Size / 2;
-        Renderer.QueueRedraw();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if (timer > DRAW_FREQ)
+        {
+            timer += (float)delta;
+            return;
+        }
+        Renderer.QueueRedraw();
     }
 }
