bf85f78c251977dd6e2be1a32990c2254fad8436
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-12 23:11:23 +0200

    chore: adjust wallrun gravity

--- scripts/entities/player/states/movement/PlayerWallrunState.cs
+++ scripts/entities/player/states/movement/PlayerWallrunState.cs
@@ -7,7 +7,7 @@ public class PlayerWallRunState : PlayerState
     public Vector3 WallNormal;
     public Vector3 WallForward;
     private float _gravityScale = -0.2f;
-    private float _decayTime = 1.5f;
+    private float _decayTime = 1f;
     private Tween _decayTween;
     public PlayerWallRunState(Player owner) : base(owner) { }
     public override void PhysicsProcess(float delta)
@@ -24,13 +24,16 @@ public class PlayerWallRunState : PlayerState
         {
             fsm.ChangeState("walljump", this); return;
         }
-        Vector3 newVelocity = new(
-            WallForward.X * _player.WallrunSpeed,
-            _player.GetGravity().Y * _gravityScale * delta,
-            WallForward.Z * _player.WallrunSpeed
-        );
-        newVelocity += -WallNormal * 3;
-        _player.Velocity = newVelocity;
+        Vector3 velocity = _player.Velocity;
+        velocity.X = WallForward.X * _player.WallrunSpeed;
+        velocity.Z = WallForward.Z * _player.WallrunSpeed;
+        velocity.Y += _player.GetGravity().Y * _gravityScale * delta;
+        GD.Print($"Adding gravity: {_player.GetGravity().Y * _gravityScale * delta}");
+
+        // Stick to wall to make it more stable
+        velocity += -WallNormal * 3; // Maybe make this a tuning variable?
+
+        _player.Velocity = velocity;
         _player.MoveAndSlide();
 
     }
@@ -41,11 +44,12 @@ public class PlayerWallRunState : PlayerState
             _side = fall.Side;
             GD.Print($"Side: {_side}");
         }
+        _gravityScale = 0f;
         _decayTween?.Kill();
         _decayTween = _player.CreateTween();
         _decayTween.SetEase(Tween.EaseType.In);
-        _decayTween.SetTrans(Tween.TransitionType.Circ);
-        _decayTween.TweenMethod(Callable.From<float>((x) => _gravityScale = x), 1, 20f, _decayTime);
+        _decayTween.SetTrans(Tween.TransitionType.Sine);
+        _decayTween.TweenMethod(Callable.From<float>((x) => _gravityScale = x), 0, 8f, _decayTime);
     }
 
     private bool IsStillOnWall()
