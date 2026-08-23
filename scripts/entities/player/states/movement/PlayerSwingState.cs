4204bd88552ad754858427048d915bf13281dfec
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-23 15:55:27 +0200

    chore: add updated swinging script

--- scripts/entities/player/states/movement/PlayerSwingState.cs
+++ scripts/entities/player/states/movement/PlayerSwingState.cs
@@ -2,47 +2,50 @@ using Godot;
 
 public class PlayerSwingState : PlayerState
 {
+    float _springStiffness = 50f;
+    float _springDamping = 5.0f;
+    float _restLengthMultiplier = 0.8f;
+    float _swingForce = 10f;
+
+    private Vector3 _anchorPoint;
+    private float _restLength;
+
     public PlayerSwingState(Player owner)
         : base(owner) { }
 
-    public override void PhysicsProcess(float delta)
+    public override void IntegrateForces(PhysicsDirectBodyState3D state)
     {
-        // Vector2 inputDir = _player.GetInputVector();
-        // Transform3D cameraTransform = _player.Camera.GlobalTransform;
-        // Vector3 moveDirection = (cameraTransform.Basis.X * inputDir.X).Normalized();
+        Vector3 direction = _anchorPoint - _player.GlobalPosition;
+        float currentLength = direction.Length();
+        if (currentLength <= 0)
+            return;
+        Vector3 ropeDirection = direction.Normalized();
+        float stretch = currentLength - _restLength;
 
-        // _player.Direction = moveDirection;
-        // _player.HorizontalSpeed = 10f;
+        if (stretch > 0)
+        {
+            float relativeVelocity = state.LinearVelocity.Dot(ropeDirection);
+
+            // apply hookes law
+            float springForceMultiplier =
+                (_springStiffness * stretch) - (_springDamping * relativeVelocity);
+            Vector3 springForce = ropeDirection * springForceMultiplier;
+            state.ApplyCentralForce(springForce);
+        }
+
+        // Handle swing
+        Vector3 inputDir = _player.GetCorrectedInput();
+        if (inputDir.Length() <= 0)
+            return;
+
+        Plane swingPlane = new Plane(ropeDirection);
+        Vector3 tangentialDirection = swingPlane.Project(inputDir).Normalized();
+        state.ApplyCentralForce(tangentialDirection * _swingForce);
     }
 
     public override void Enter(State previous = null)
     {
-        // _player.Freeze = true;
-        // _player.ApplyCentralImpulse(Vector3.Forward * 5f);
-        // _player.LockRotation = false;
-    }
-
-    public override void Exit()
-    {
-        // _player.Freeze = true;
-
-        // // Tween rotation back to normal
-        // _startBasis = _player.GlobalBasis;
-        // Tween tween = _player.CreateTween();
-        // tween.TweenMethod(Callable.From((float w) => InterpolateBasis(w)), 0.0, 1.0f, 0.3f);
-        // tween.TweenCallback(Callable.From(() => ResetRigidbody()));
-    }
-
-    Basis _startBasis;
-
-    private void InterpolateBasis(float weight)
-    {
-        _player.GlobalBasis = _startBasis.Slerp(Basis.Identity, weight);
-    }
-
-    private void ResetRigidbody()
-    {
-        _player.Freeze = false;
-        _player.LockRotation = true;
+        _anchorPoint = _player.Whip.AnchorPoint;
+        _restLength = _player.GlobalPosition.DistanceTo(_anchorPoint) * _restLengthMultiplier;
     }
 }
