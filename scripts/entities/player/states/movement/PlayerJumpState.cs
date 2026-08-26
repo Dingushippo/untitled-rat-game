0c093fba46941acf2ea17af8d9317d34157703f5
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-26 18:03:13 +0200

    chore: allow for custom force and direction in jump state

--- scripts/entities/player/states/movement/PlayerJumpState.cs
+++ scripts/entities/player/states/movement/PlayerJumpState.cs
@@ -3,12 +3,36 @@ using Godot;
 public class PlayerJumpState : PlayerState
 {
     public PlayerJumpState(Player owner)
-        : base(owner) { }
+        : base(owner)
+    {
+        Reset();
+    }
+
+    private Vector3 _launchDirection;
+    private float _launchForce;
+
+    private void Reset()
+    {
+        _launchDirection = Vector3.Up;
+        _launchForce = _player.Tuning.JumpForce;
+    }
+
+    public void Configure(Vector3 direction, float force)
+    {
+        _launchDirection = direction;
+        _launchForce = force;
+    }
 
     public override void Enter(State previous = null)
     {
+        if (previous is not PlayerSwingState)
+        {
+            GD.Print("Not swing");
+            Reset();
+        }
         _player.StickToFloor = false;
-        Vector3 jumpImpulse = Vector3.Up * _player.Tuning.JumpForce;
+        Vector3 jumpImpulse = _launchDirection * _launchForce;
+        GD.Print($"Adding jump impulse: {jumpImpulse}");
         _player.SetImpulse(jumpImpulse);
         fsm.ChangeState<PlayerFallingState>(this);
     }
