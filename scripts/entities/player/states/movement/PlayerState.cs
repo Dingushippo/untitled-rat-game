26b3ab03a27ecd9707baf9c2a7a88728db300b76
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-29 16:12:50 +0200

    chore: update base player state

--- scripts/entities/player/states/movement/PlayerState.cs
+++ scripts/entities/player/states/movement/PlayerState.cs
@@ -1,28 +1,30 @@
+
+
 using Godot;
 
-public class PlayerState : TypedState<PlayerState>
+public partial class PlayerState : TypedState<PlayerState>
 {
     private protected Player _player;
 
-    public PlayerState(Player owner)
+    protected Vector3 _velocity => _player.Velocity;
+    protected void SetVelocity(Vector3 velocity)
     {
-        _player = owner;
+        _player.Velocity = velocity;
     }
 
-    // Setting the base movement integration here, because most movement states reuse the same thing
-    public override void IntegrateForces(PhysicsDirectBodyState3D state)
+    protected void MoveAndSlide() => _player.MoveAndSlide();
+
+    public override void Init(Node owner, HierarchicalStateMachine<PlayerState> stateMachine, State parent)
     {
-        _player.CheckOnFloor(state);
-        _player.CheckOnWall(state);
-        _player.HandleDirectionalMovement(state);
-        _player.HandleImpulse(state);
+        base.Init(owner, stateMachine);
+        _player = (Player)owner;
     }
 }
 
 /*
 using Godot;
 
-public class PlayerNewState : PlayerState
+public partial class PlayerNewState : PlayerState
 {
     public PlayerNewState(Player owner) : base(owner) { }
     public override void PhysicsProcess(float delta) { }
