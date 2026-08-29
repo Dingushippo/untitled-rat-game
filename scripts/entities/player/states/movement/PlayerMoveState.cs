9c483954114cd3bfb072a87114a0986ca82bb346
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-08-29 16:13:02 +0200

    chore: update PlayerMoveState

--- scripts/entities/player/states/movement/PlayerMoveState.cs
+++ scripts/entities/player/states/movement/PlayerMoveState.cs
@@ -1,68 +1,43 @@
 using Godot;
 
-public class PlayerMoveState : PlayerState
+public partial class PlayerMoveState : PlayerState
 {
-    public PlayerMoveState(Player owner)
-        : base(owner) { }
+    [Export] public float Speed;
+    [Export] public float Acceleration;
+    [Export] public float Deceleration;
 
+    private float _speed;
     public override void PhysicsProcess(float delta)
     {
-        if (!_player.IsOnFloor)
+        if (_parent is not PlayerGroundedState grounded)
         {
-            _hfsm.ChangeState<PlayerFallingState>(this);
-            return;
+            GD.PushError($"{this} is not a child of PlayerGroundedState"); return;
         }
-        _player.Direction = _player.GetCorrectedInput();
 
-        if (_player.LinearVelocity == Vector3.Zero && _player.Direction == Vector3.Zero)
+        grounded.PhysicsProcess(delta);
+
+        float desiredSpeed = Speed;
+        float accel = Acceleration;
+
+        if (grounded.Direction == Vector3.Zero)
+        {
+            desiredSpeed = 0f;
+            accel = Deceleration;
+        }
+
+        _speed = Mathf.MoveToward(_speed, desiredSpeed, accel * delta);
+        SetVelocity(grounded.Direction * _speed);
+        MoveAndSlide();
+
+        if (_velocity.IsEqualApprox(Vector3.Zero))
         {
             _hfsm.ChangeState<PlayerIdleState>();
-            return;
         }
-
-        _player.HorizontalAccel =
-            _player.Direction == Vector3.Zero
-                ? _player.Tuning.Acceleration
-                : _player.Tuning.Deceleration;
-
-        if (_player.CrouchComponent.IsCrouching)
-            _player.HorizontalSpeed = _player.Tuning.CrouchSpeed;
-        else if (Input.IsActionPressed("sprint"))
-            _player.HorizontalSpeed = _player.Tuning.SprintSpeed;
-        else
-            _player.HorizontalSpeed = _player.Tuning.Speed;
     }
 
     public override void Exit() { }
 
     public override void Enter(State previous = null)
     {
-        _player.StickToFloor = true;
-    }
-
-    public override void HandleInput(InputEvent @event)
-    {
-        if (@event.IsActionPressed("jump"))
-        {
-            _hfsm.ChangeState<PlayerJumpState>(this);
-            return;
-        }
-        if (@event.IsActionPressed("sprint"))
-        {
-            _player.CrouchComponent.TryStand();
-        }
-        if (
-            @event.IsActionPressed("crouch")
-            && Input.IsActionPressed("sprint")
-            && !_player.CrouchComponent.IsCrouching
-        )
-        {
-            _hfsm.ChangeState<PlayerSlideState>(this);
-        }
-        if (@event.IsActionPressed("crouch") && _player.IsOnFloor && _player.GetFloorAngle() != 0)
-        {
-            if (_player.Velocity.Y < 0)
-                _hfsm.ChangeState<PlayerSlideState>(this);
-        }
     }
 }
