1878631fd12e0fb00ed40f8926f7f367fd0b4dc7
Dingushippo <43849676+Dingushippo@users.noreply.github.com>
2026-09-01 22:38:37 +0200

    chore: clean up cast manager

--- scripts/entities/player/spells/CastManager.cs
+++ scripts/entities/player/spells/CastManager.cs
@@ -1,5 +1,4 @@
 using Godot;
-using Godot.Collections;
 
 [GlobalClass]
 public partial class CastManager : Node
@@ -8,28 +7,15 @@ public partial class CastManager : Node
     [Export] public SpellData[] SpellResources;
     [Export] public Node3D CastNode;
 
-    // private List<Spell> _activeSpells;
-    // private Spell _currentSpell;
-
-    private float _chargeTimer;
-    private bool _isCharging;
-
-    public override void _PhysicsProcess(double delta)
-    {
-
-    }
-
-    public void Cast(SpellData spell, Vector3 targetPosition, Array<Node3D> targetNode = null) // on release
+    public void Cast(SpellData spell) // on release
     {
         if (spell.SpellScene == null)
             return;
 
-
-        SpellPayload payload = new(CastNode, targetPosition);
+        SpellPayload payload = new(CastNode);
 
         if (spell.SpellScene.Instantiate() is SpellBase spellInstance)
         {
-            GD.Print("test");
             GetTree().CurrentScene.AddChild(spellInstance);
             spellInstance.Initialize(CastNode, payload);
         }
